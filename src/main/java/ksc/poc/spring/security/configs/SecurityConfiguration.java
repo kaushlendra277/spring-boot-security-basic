f96b4b21da2128c4546e2afa0c839fc90b6d5b7b
kaushlendra277 <kaushlendra277@gmail.com>
2019-11-04 21:08:31 +0530

    added authorization to spring boot app

--- src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
+++ src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ksc.poc.spring.security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	// AUTHENTICATION
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
@@ -19,11 +21,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.inMemoryAuthentication()
 			.withUser("ksc2").password("admin2").roles("USER")
 			.and()
-			.withUser("ksc3").password("admin3").roles("USER")
+			.withUser("ksc3").password("admin3").roles("ADMIN")
 			;
 		
 	}
 	
+	
+	// AUTHORIZATION
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		// super.configure(http);
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN") // admin apis- all path in the current level for ADMIN role user
+		.antMatchers("/user").hasAnyRole("USER","ADMIN") // user apis- all path in the current level for USER and ADMIN role user
+		.antMatchers("/").permitAll() // all path in the current level of any role
+		// .antMatchers("/**") // all path in the current level and sub path
+		// .hasAnyRole("ADMIN")
+		.and().formLogin(); // ?
+	}
+
+
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		// NoOpPasswordEncoder takes userid and password as String
--- src/main/java/ksc/poc/spring/security/resources/HomeResource.java
+++ src/main/java/ksc/poc/spring/security/resources/HomeResource.java
@@ -6,8 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
+	// Whitelist/public API
+	// should be accessed by all un-authenticated user
 	@GetMapping
 	public String home() {
 		return ("<h1> Hello Principal </h1>");
+	}
+
+	// should be accessed by all authenticated user with the role ADMIN or USER
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1> Hello User </h1>");
+	}
+
+	// should be accessed by all authenticated user with the role ADMIN
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1> Hello Admin </h1>");
 	}
 }
