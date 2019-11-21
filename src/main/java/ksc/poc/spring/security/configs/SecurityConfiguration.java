d7b49db4781a902634bb2268da60f3db60bc7f9f
kaushlendra277 <kaushlendra277@gmail.com>
2019-11-21 13:54:31 +0530

    added branch for encoded password using BCryptPasswordEncoder

--- src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
+++ src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -17,17 +17,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// super.configure(auth);
 		auth
 			.inMemoryAuthentication()
-			.withUser("ksc2").password("admin2").roles("USER")
+			.withUser("ksc2").password(passwordEncoder().encode("admin2")).roles("USER")
 			.and()
-			.withUser("ksc3").password("admin3").roles("USER")
+			.withUser("ksc3").password(passwordEncoder().encode("admin3")).roles("USER")
 			;
 		
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		// NoOpPasswordEncoder takes userid and password as String
-		return NoOpPasswordEncoder.getInstance();
+		// NoOpPasswordEncoder encodes string password
+		//return NoOpPasswordEncoder.getInstance();
+		// BCryptPasswordEncoder password as String
+		return new BCryptPasswordEncoder();
 	}
 
 }
