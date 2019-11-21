6d31cce25e5fb677fbbfa3b1e08dfce7f2f43fee
kaushlendra277 <kaushlendra277@gmail.com>
2019-11-21 17:09:33 +0530

    added permission based api and user

--- src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
+++ src/main/java/ksc/poc/spring/security/configs/SecurityConfiguration.java
@@ -9,10 +9,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.fasterxml.jackson.databind.introspect.WithMember;
-
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	private final String ACCESS_TEST1_AUTHORITY = "ACCESS_TEST1";
+	private final String ACCESS_TEST2_AUTHORITY = "ACCESS_TEST2";
 
 	// AUTHENTICATION
 	@Override
@@ -22,11 +23,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// super.configure(auth);
 		auth
 			.inMemoryAuthentication()
-			.withUser("user").password("admin").roles("USER")
+			.withUser("user").password("admin").authorities(ACCESS_TEST1_AUTHORITY).roles("USER") // even on adding ACCESS_TEST1_AUTHORITY spring is not able to call "/user" api ?
+			.and()
+			.withUser("test1").password("admin").roles("USER").authorities(ACCESS_TEST1_AUTHORITY) // even on adding .role spring is not able to call "/user" api ?
+			.and()
+			.withUser("test2").password("admin").roles("USER").authorities(ACCESS_TEST1_AUTHORITY, ACCESS_TEST2_AUTHORITY) // even on adding .role spring is not able to call "/user" api ?
 			.and()
 			.withUser(User
 						.withUsername("admin")
 						.password("admin")
+						.authorities(ACCESS_TEST1_AUTHORITY,ACCESS_TEST2_AUTHORITY)
 						.roles("ADMIN"))
 			;
 		
@@ -41,10 +47,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/admin").hasRole("ADMIN") // admin apis- all path in the current level for ADMIN role user
 		.antMatchers("/user").hasAnyRole("USER","ADMIN") // user apis- all path in the current level for USER and ADMIN role user
+		.antMatchers("/test1").hasAuthority(ACCESS_TEST1_AUTHORITY) // only user with this authority can access this endpoint
+		.antMatchers("/test2").hasAuthority(ACCESS_TEST2_AUTHORITY)// only user with this authority can access this endpoint
 		.antMatchers("/").permitAll() // all path in the current level of any role
 		// .antMatchers("/**") // all path in the current level and sub path
 		// .hasAnyRole("ADMIN")
-		.and().formLogin(); // ?
+		.and().formLogin()
+		// .httpBasic() -- it gives basic authentication
+		// .formLogin() - it gives session kind of funtionality[PREFERRED since it provide logout] // ?
+		;
 	}
 
 	@Bean
--- src/main/java/ksc/poc/spring/security/resources/HomeResource.java
+++ src/main/java/ksc/poc/spring/security/resources/HomeResource.java
@@ -24,4 +24,21 @@ public class HomeResource {
 	public String admin() {
 		return ("<h1> Hello Admin </h1>");
 	}
+
+	/***
+	 * 
+	 * test1, test2 apis are for permisson based authorization
+	 */
+	
+	// should be accessed by all authenticated user with the permission ACCESS_TEST1
+	@GetMapping("/test1")
+	public String test1() {
+		return ("<h1> Test 1 </h1>");
+	}
+
+	// should be accessed by all authenticated user with the permission ACCESS_TEST2
+	@GetMapping("/test2")
+	public String test2() {
+		return ("<h1> Test 2 </h1>");
+	}
 }
